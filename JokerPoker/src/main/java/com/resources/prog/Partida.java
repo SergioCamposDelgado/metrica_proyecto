9b23e1ffe0fea29935b03cf2e55ecd9c9375496d
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-05-13 13:02:38 +0200

    JokerPoker/Añadido la clase partida

---
+++ JokerPoker/src/main/java/com/resources/prog/Partida.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.resources.prog;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Sergio Campos Delgado
+ */
+public class Partida {
+    protected int id;
+    protected Usuario user;
+    protected boolean userWins;
+    protected Date date;
+
+    public Partida(int id, Usuario user, boolean userWins, Date date) {
+        this.id = id;
+        this.user = user;
+        this.userWins = userWins;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public boolean isUserWins() {
+        return userWins;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    public void setUserWins(boolean userWins) {
+        this.userWins = userWins;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    
+    
+}
