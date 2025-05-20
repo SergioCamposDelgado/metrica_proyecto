d6ee1e8b4bc302bc380e691495bc78433093df78
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-05-20 17:16:50 +0200

    Añadido la clase de modificar informacion

--- JokerPoker/src/main/java/com/jframe/Juego.java
+++ JokerPoker/src/main/java/com/jframe/Juego.java
@@ -187,7 +187,7 @@ public class Juego extends javax.swing.JFrame {
             .addGap(0, 300, Short.MAX_VALUE)
         );
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("JokerPoker");
         setResizable(false);
 
